8515db6ae463fee47582afbbefb67bfa6f1ebdbe
Charlie Curtis <charlie-curtis@outlook.com>
2022-10-22 18:47:42 -0400

    cleanup egg drop

--- DP/EggDroppingProblem.java
+++ DP/EggDroppingProblem.java
@@ -1,49 +1,32 @@
 package DP;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 
 
+/**
+ * There are two eggs in this problem.
+ */
 public class EggDroppingProblem {
 
-  private HashMap<Integer, Integer> minTosses = new HashMap<Integer, Integer>();
+  private final HashMap<Integer, Integer> memo = new HashMap<>();
 
   public int getMinimumTosses(int floors) {
 
-    ArrayList<Integer> valueHolder = new ArrayList<Integer>();
+    if (floors == 0) return 0;
+    if (floors == 1) return 1;
+    if (memo.containsKey(floors)) return memo.get(floors);
+
+
+    int minValue = Integer.MAX_VALUE;
     for (int i = 1; i <= floors; i++) {
-      //egg breaks
-      int case1 = (i - 1);
-      //egg doesn't break;
-      int case2 = 0;
+      //If the egg breaks on this floor, then we need to check from 1 to i-1 to figure out the exact floor
+      int optionA = (i - 1);
+      //If the egg doesn't break, then we need to repeat the problem from i to n
+      int optionB = getMinimumTosses(floors - i);
 
-      if (minTosses.containsKey(floors - i)) {
-        if ((floors - i) > 0) {
-          case2 = minTosses.get(floors - i);
-        } else {
-          case2 = 0;
-        }
-      } else {
-        if ((floors - i) > 0) {
-          case2 = getMinimumTosses(floors - i);
-          this.minTosses.put(floors - i, case2);
-        } else {
-          case2 = 0;
-        }
-      }
-      valueHolder.add(Math.max(case1 + 1, case2 + 1));
+      minValue = Math.min(minValue, 1 + Math.max(optionA, optionB));
     }
-    return minValueCalculator((valueHolder));
-  }
-
-  public int minValueCalculator(ArrayList<Integer> list) {
-    int minVal = list.size() > 0 ? list.get(0) : 0;
-    for (int i = 0; i < list.size(); i++) {
-      if (list.get(i) < minVal) {
-        minVal = list.get(i);
-      }
-    }
-    return minVal;
+    memo.put(floors, minValue);
+    return minValue;
   }
 }
 
--- DP/EggDroppingProblemTest.java
+++ DP/EggDroppingProblemTest.java
@@ -1,14 +1,17 @@
 package DP;
 
-import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class EggDroppingProblemTest {
 
   public static void main(String[] args) {
 
     EggDroppingProblem test = new EggDroppingProblem();
-    for (int i = 0; i < 1000; i++) {
-      System.out.println("floor: " + i + " , tosses: " + test.getMinimumTosses(i));
-    }
+    IntStream.rangeClosed(0, 1000).forEach(
+      i-> {
+        int answer = test.getMinimumTosses(i);
+        System.out.printf("Floor: %d, tosses: %d%n", i, answer);
+      }
+    );
   }
 }
