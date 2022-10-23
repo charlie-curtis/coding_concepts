5698cc27f225deef78db0dd22cf504188d4c15b5
Charlie Curtis <charlie-curtis@outlook.com>
2022-10-22 20:08:03 -0400

    playing around

--- Miscellanous/ByteArray.java
+++ Miscellanous/ByteArray.java
@@ -4,7 +4,8 @@ public class ByteArray {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    byte[] data = {1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1};
+    //byte[] data = {1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1};
+    byte[] data = {48, 20, 111, 65, -10, 99, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1};
 
     System.out.println(new String(data));
   }
